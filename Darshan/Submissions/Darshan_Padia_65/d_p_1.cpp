1dbea40835c22a841c5958f2183b92342e076092
MananTheCoder <manangoyaljee@gmail.com>
2024-10-08 01:13:16 +0530

    Stashing the change in testcases

--- Darshan/Submissions/Darshan_Padia_65/d_p_1.cpp
+++ Darshan/Submissions/Darshan_Padia_65/d_p_1.cpp
@@ -2,13 +2,17 @@
 // should pass all
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    if(n&1){
-        cout<<"NO";
-    }else{
-        cout<<"YES";
+    cin >> n;
+    if (n & 1)
+    {
+        cout << "NO";
     }
-    return 0;
+    else
+    {
+        cout << "YES";
+    }
+    return 1;
 }
